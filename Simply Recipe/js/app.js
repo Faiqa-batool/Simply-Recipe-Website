0c99231c7488fe7224094ed4555e40002a8a357b
Faiqa-batool <115587465+Faiqa-batool@users.noreply.github.com>
2023-05-20 15:57:55 +0500

    Add files via upload

---
+++ Simply Recipe/js/app.js
@@ -0,0 +1,20 @@
+const getElement = (selector) => {
+  const element = document.querySelector(selector)
+
+  if (element) return element
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  )
+}
+
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+
+navBtnDOM.addEventListener('click', () => {
+  links.classList.toggle('show-links')
+})
+
+// current date
+const date = getElement('#date')
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
